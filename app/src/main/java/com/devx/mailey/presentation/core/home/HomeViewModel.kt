4453ba62fc35e4c8022a0b98334536a653b8e5f8
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-03-17 17:14:28 +0100

    fix HomeViewModel.kt

--- app/src/main/java/com/devx/mailey/presentation/core/home/HomeViewModel.kt
+++ app/src/main/java/com/devx/mailey/presentation/core/home/HomeViewModel.kt
@@ -16,14 +16,8 @@ class HomeViewModel @Inject constructor(private val databaseRepository: Database
         getRoomById()
     }
     fun createRoom() {
-        val message = Message("text", "vadyum")
-        val message2 = Message("text2", "vadym2")
-        val room = Room(listOf(message, message2), "test", listOf("1"))
-        databaseRepository.createRoom(room)
     }
 
     private fun getRoomById() = viewModelScope.launch {
-        val result = databaseRepository.getRoomById("test")
-        Log.d("vadim", result.result.toString())
     }
 }
